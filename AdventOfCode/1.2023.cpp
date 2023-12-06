d344a7875f33d2150ff94e1b1d914658cbfd48b2
CrageJ <59778978+CrageJ@users.noreply.github.com>
2023-12-06 14:51:24 +1030

    yjgh

--- AdventOfCode/1.2023.cpp
+++ AdventOfCode/1.2023.cpp
@@ -27,24 +27,15 @@ void Solution::part1(){
         }
         sum += l*10 + r;
     }
-
     cout<<"Part 1: "<<sum<<endl;
-    
 }
 
-    unordered_map<string,int> sset = {{"one",1},{"two",2},{"three",3},{"four",4},{"five",5},{"six",6},{"seven",7},{"eight",8},{"nine",9}};
+unordered_map<string,int> sset = {{"one",1},{"two",2},{"three",3},{"four",4},{"five",5},{"six",6},{"seven",7},{"eight",8},{"nine",9}};
+
 int findWord(string s){
-    //one"," two"," three"," four"," five"," six"," seven"," eight"," and nine
-
-   // cout<<s<<endl;
-
     int size= s.length();
-
-
     for (int l = 0;l<size-2;l++){
         for(int r = 3; r<= 5;r++){
-            
-            //cout<<'l';
             string sub = s.substr(l,r);
             if (sset.count(sub) != 0){
                 return sset[sub];
@@ -52,8 +43,6 @@ int findWord(string s){
 
         }
     }
-    // s contains word
-
     return -1;
 }
 
@@ -65,37 +54,30 @@ void Solution::part2(){
         int size = line.length();
         int l = 0;
         int r = 0;
-        //cout<<"LINE: "<<line<<endl;
         for (int i = 0;i< size;i++){
             if ('0'<= line[i] && line[i] <='9'){
                 l = line[i]-'0';
                 break;
             }
             sub = line.substr(0,i+1);
-            //cout<<"i: "<<"0 "<<i<<' '<<sub<<endl;
             int wrd = findWord(sub);
             if (wrd!=-1){
                 l= wrd;
                 break;
             }
         }
-        //cout<<'<';
         for (int i = size-1;i>=0;i--){
             if ('0'<= line[i] && line[i] <='9'){
                 r = line[i]-'0';
                 break;
             }
-
-            //cout<<sub<<endl;
             sub = line.substr(i,size-i);
-            //cout<<"i: "<<i<<' '<<size-i<<' '<<sub<<endl;
             int wrd = findWord(sub);
             if (wrd!=-1){
                 r= wrd;
                 break;
             }
         }
-
         sum += l*10 + r;
     }
         cout<<"Part 2: "<<sum<<endl;
@@ -104,9 +86,32 @@ void Solution::part2(){
 
 
 int main(void){
+    auto precomp = std::chrono::high_resolution_clock::now();
+
     Solution s("1.2023.1",Solution::readType::lines);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    auto readin = std::chrono::duration_cast<std::chrono::microseconds>(start - precomp);
+    cout<<"--------------Read time: "<<readin.count()/1e3<<" milliseconds------------------"<<endl;
+    start = std::chrono::high_resolution_clock::now();
+
     s.part1();
+    auto p1finish =  std::chrono::high_resolution_clock::now();
+
     s.part2();
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto p1duration = std::chrono::duration_cast<std::chrono::microseconds>(p1finish - start);
+    auto p2duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - p1finish);
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    auto totalduration = std::chrono::duration_cast<std::chrono::microseconds>(stop - precomp);
+    cout<< "Part 1 Execution Time: "<<p1duration.count()/1e3<<" milliseconds"<<endl;
+    cout<< "Part 2 Execution Time: "<<p2duration.count()/1e3<<" milliseconds"<<endl;
+    cout << "Execution Time: " << duration.count() / 1e3 << " milliseconds" << endl;
+    cout << "Total Time: " << totalduration.count() / 1e3 << " milliseconds" << endl;
+
+
 
     return 0;
 };
---
+++ AdventOfCode/5.2023.cpp
@@ -0,0 +1,213 @@
+#include <bits/stdc++.h>
+#include "src/solutionTemplate.hpp"
+#include "src/solutionTemplate.cpp"
+
+using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+template <typename T>
+struct PairHash {
+    std::size_t operator()(const std::pair<T, T>& p) const {
+        auto hash1 = std::hash<T>{}(p.first);
+        auto hash2 = std::hash<T>{}(p.second);
+        return hash1 ^ hash2;
+    }
+};
+
+
+void Solution::part1() {
+    
+
+    vector<ll> seeds;
+    //cout<<p1.size()<<' '<<p1[0].size();
+
+
+    for (int i = 1;i<(int)p1[0].size();i++){
+        seeds.push_back(stoll(p1[0][i]));
+    }
+    vector<bool> processed(seeds.size(),false);
+
+    for (int r = 3;r<p1.size();r++) {
+        auto row = p1[r];
+        if (row.size()<3){
+            r=r+1;
+            processed.resize(0,false);
+            processed.resize(seeds.size(),false);
+            continue;
+        }
+        ll dest = stoll(row[0]);
+        ll src = stoll(row[1]);
+        ll range = stoll(row[2]);
+        
+        for (int i = 0 ;i< seeds.size();i++){
+            ll seed = seeds[i]; 
+            if (processed[i] == false && src <= seed && seed <=src+range-1){
+
+                seeds[i] = dest + (seed-src);
+                //cout<<"Seed: "<<seed<<" "<<seeds[i];
+                processed[i] = true;
+            }
+            else{
+                seeds[i] = seed;
+            }
+          //  cout<<seed<<' ';
+        }
+        //cout<<endl;
+
+    }
+    ll res =  *min_element(seeds.begin(),seeds.end());
+    cout<<"Part 1: "<< res<<endl;
+}
+
+struct seedRange{
+    ll startingSeed;
+    ll range;
+    
+    seedRange(ll ss,ll r) {
+        this->startingSeed = ss;
+        this->range = r;
+    }
+};
+
+ll intersectingSeedRanges(seedRange a, seedRange b){
+    ll al = a.startingSeed;
+    ll bl = b.startingSeed;
+    ll ar = a.startingSeed+a.range;
+    ll br = b.startingSeed+b.range;
+
+
+    if (br <= al || ar <= bl){
+            return 0; //////////?
+    }
+        
+    return abs(min(ar,br) - max(al,bl)); // plus 1?
+    
+    
+}
+
+
+
+void Solution::part2() {
+    
+
+
+    list<seedRange> seeds;
+    list<seedRange> newSeeds;
+    
+
+    for (int i = 2;i<p2[0].size();i+=2){
+        seeds.push_back(seedRange(stoll(p2[0][i-1]),stoll(p2[0][i])));
+    }
+
+
+    for (int r = 1;r<p2.size();r++) {
+        auto row = p2[r];
+        if (row.size()<3){
+            r=r+1;
+            cout<<"stage end: ";
+            
+            seeds.splice(seeds.end(),newSeeds);
+            for (auto s : seeds){
+            cout<<"{"<<s.startingSeed<<' '<<s.range<<'}'<<' ';
+            }
+            cout<<endl;
+            continue;
+        }
+
+        ll dest = stoll(row[0]);
+        ll src = stoll(row[1]);
+        ll range = stoll(row[2]);
+        cout<<dest<<' '<<src<<' '<<range<<endl;
+        
+        cout<<"To process: ";
+        for (auto s : seeds){
+            cout<<"{"<<s.startingSeed<<' '<<s.range<<'}'<<' ';
+        }
+        
+
+        cout<<endl<<"To next phase: ";
+        for (auto s : newSeeds){
+            cout<<"{"<<s.startingSeed<<' '<<s.range<<'}'<<' ';
+        }
+        cout<<endl;
+        
+
+
+        seedRange transformer = seedRange(src,range);
+        
+
+        auto it = seeds.begin();
+        
+        while (it != seeds.end()){
+            auto& seed = *it;
+           // cout<< seed.startingSeed<<' ';
+
+            ll area = intersectingSeedRanges(transformer,seed);
+            if (area == 0) {
+                it = next(it);  // Move to the next element
+                continue;
+            }
+            
+
+            int lRange = transformer.startingSeed - seed.startingSeed ;
+            int rRange = seed.startingSeed+seed.range - (transformer.startingSeed+transformer.range); 
+            if (lRange > 0){
+                seeds.push_back(seedRange(seed.startingSeed,lRange));// non transformed
+            }
+            if (rRange > 0){
+                seeds.push_back(seedRange(transformer.startingSeed+transformer.range,rRange)); // non transformed
+            }
+            ll newPos = dest;
+            ll newRange = area;
+                
+            newSeeds.push_back(seedRange(newPos,newRange));
+            
+            it = seeds.erase(it);
+            
+        }
+
+        
+        
+    }
+    for (seedRange s : seeds){
+        newSeeds.push_back(s);
+    }
+    ll res = (ll)INT_MAX;
+    for (auto s : newSeeds){
+       res = min(s.startingSeed,res);
+    }
+    std::cout<<"Part 2: "<< res<<endl; 
+}
+
+int main(void){
+    auto precomp = std::chrono::high_resolution_clock::now();
+
+    cout<<intersectingSeedRanges(seedRange(79,14),seedRange(50,52))<<endl;
+    cout<<intersectingSeedRanges(seedRange(0,3),seedRange(1,2))<<endl;
+
+    Solution s("test1", Solution::readType::spaced);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    auto readin = std::chrono::duration_cast<std::chrono::microseconds>(start - precomp);
+    cout << "--------------Read time: " << readin.count() / 1e3 << " milliseconds------------------" << endl;
+   
+    start = std::chrono::high_resolution_clock::now();
+
+    s.part1();
+    auto p1finish = std::chrono::high_resolution_clock::now();
+    s.part2();
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto p1duration = std::chrono::duration_cast<std::chrono::microseconds>(p1finish - start);
+    auto p2duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - p1finish);
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    auto totalduration = std::chrono::duration_cast<std::chrono::microseconds>(stop - precomp);
+    cout << "Part 1 Execution Time: " << p1duration.count() / 1e3 << " milliseconds" << endl;
+    cout << "Part 2 Execution Time: " << p2duration.count() / 1e3 << " milliseconds" << endl;
+    cout << "Execution Time: " << duration.count() / 1e3 << " milliseconds" << endl;
+    cout << "Total Time: " << totalduration.count() / 1e3 << " milliseconds" << endl;
+
+    return 0;
+}
